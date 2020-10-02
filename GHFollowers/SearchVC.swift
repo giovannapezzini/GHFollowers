b28c6948cec433c15196c6e3ea4fba0af5350670
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-02 02:26:52 -0300

    Create Search View Controller

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Giovanna Pezzini on 02/10/20.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
