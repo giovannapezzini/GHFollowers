d66ad1e56a3a69d30da59b3168d2dfaad518c4bf
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-02 02:26:40 -0300

    Create FavoritesList View Controller

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Giovanna Pezzini on 02/10/20.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
