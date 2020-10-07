edef8afa57ce07e68a50e5bbf3cc418f6c5652df
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-07 02:57:57 -0300

    Re-Indent

--- GHFollowers/GFTitleLabel.swift
+++ GHFollowers/GFTitleLabel.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class GFTitleLabel: UILabel {
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
         
     }
-        
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
