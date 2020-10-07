516f15998d2b169163caa46ad0f64d8d3b07d604
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-07 03:38:31 -0300

    Fix textAlignment on init

--- GHFollowers/GFTitleLabel.swift
+++ GHFollowers/GFTitleLabel.swift
@@ -12,14 +12,13 @@ class GFTitleLabel: UILabel {
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
-        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(textAling: NSTextAlignment, fontSize: CGFloat) {
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
         super.init(frame: .zero)
         self.textAlignment = textAlignment
         self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
