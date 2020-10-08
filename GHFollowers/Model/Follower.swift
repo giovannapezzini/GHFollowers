326c503378e6777f0eddd5486436c5e7881fe727
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-08 18:23:54 -0300

    Add Follower model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Giovanna Pezzini on 07/10/20.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
