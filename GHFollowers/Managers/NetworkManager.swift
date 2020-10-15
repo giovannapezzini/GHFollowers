82f5222d1bfbb7e0d1795cec9df5ea1dc4208187
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-15 14:32:40 -0300

    Add NSCache

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,12 +5,13 @@
 //  Created by Giovanna Pezzini on 08/10/20.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
-    
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
+
     private init() {}
     
     func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
