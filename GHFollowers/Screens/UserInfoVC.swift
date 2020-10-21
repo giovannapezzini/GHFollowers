36686ccb4cddfc174109fa32cc2e6f14e2b13dba
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-21 18:27:03 -0300

    Add UserInfoVC

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,24 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Giovanna Pezzini on 21/10/20.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
