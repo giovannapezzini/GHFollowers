477342885f2b1e7ad824164e80bbd64f61e9baa8
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-23 00:16:59 -0300

    Add Constants file

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Giovanna Pezzini on 22/10/20.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
