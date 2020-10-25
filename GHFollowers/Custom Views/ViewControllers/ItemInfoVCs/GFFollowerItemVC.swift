061da3ac3e4c6488633199a9782d8591d33bc7bb
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-25 20:02:00 -0300

    Add GFFollowerItemVC

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Giovanna Pezzini on 25/10/20.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, with: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, with: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
