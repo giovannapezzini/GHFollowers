a81d5c3dac7d76a1569e419557d1ae6970d81adf
giovannapezzini <giovannarpezzini@gmail.com>
2020-10-25 20:03:33 -0300

    Add access to user info

--- GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
@@ -10,10 +10,21 @@ import UIKit
 class GFItemInfoVC: UIViewController {
     
     let stackView = UIStackView()
-    let itemInfoView = GFItemInfoView()
+    let itemInfoViewOne = GFItemInfoView()
     let itemInfoViewTwo = GFItemInfoView()
     let actionButton = GFButton()
-
+    
+    var user: User!
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroundView()
@@ -30,7 +41,7 @@ class GFItemInfoVC: UIViewController {
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
         
-        stackView.addArrangedSubview(itemInfoView)
+        stackView.addArrangedSubview(itemInfoViewOne)
         stackView.addArrangedSubview(itemInfoViewTwo)
     }
     
